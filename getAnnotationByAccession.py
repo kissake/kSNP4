8a8b411cf4ea2b5c423a76ae879f6be48fc1eac0
maps@kissake.net <maps@kissake.net>
2022-08-17 16:21:47 -0400

    Added the ability to recover from a failed accession ID lookup, added the ability to return genome data (fasta)
    --HG--
    branch : makefile

--- getAnnotationByAccession.py
+++ getAnnotationByAccession.py
@@ -21,6 +21,25 @@ import logging as logging
 # TODO - JN
 # import ksnpConfig
 
+####################################
+####################################
+###
+###
+### Module / global variables
+###
+###
+####################################
+####################################
+
+# Static
+
+annotationData = 'gb'
+genomeData = 'fasta'
+cacheLocation = {
+    annotationData: 'GbkCache',
+    genomeData: 'GenomeCache'
+}
+
 
 ####################################
 ####################################
@@ -36,14 +55,16 @@ import logging as logging
 # The override option to this function can be used to cause the parsing of a provided list
 # instead of the programs passed options, for testing.
 def parseCommandline(override=None):
-    parser = argparse.ArgumentParser(description='Return genome accession data for given accession IDs')
+    parser = argparse.ArgumentParser(description='Return genome data for given accession IDs')
     parser.add_argument('accessionList', metavar='accessionID', nargs='+',
                         help='Accession IDs to return.')
     parser.add_argument('--debug', action='store_true', help='Output diagnostic data to the screen using STDERR')
     # The default will eventually be configurable with a config file.
-    parser.add_argument('--cachedir', default=os.environ.get('HOME','') + '/kSNP/GbkCache/', help='Override the directory to use for caching data')
+    parser.add_argument('--cachedir', default=os.path.join(os.environ.get('HOME',''),'kSNP'), help='Override the directory to use for caching data')
     # The default will eventually be configurable with a config file.
     parser.add_argument('--maxcachedays', type=int, default=90, help='Override the maximum age for cached data')
+    parser.add_argument('--annotation', dest='responses', action='append_const', const=annotationData, help='Output annotation data for the accession ID if available.')
+    parser.add_argument('--genome', dest='responses', action='append_const', const=genomeData, help='Output genome data for the accession ID if available.')
 
     if override is None:
         return parser.parse_args()
@@ -51,45 +72,78 @@ def parseCommandline(override=None):
         return parser.parse_args(override)
     
     
-def cacheFileName(accession, cacheDir):
-    # This shouldn't be just a string concatenation; what about directory separators?
-    return cacheDir + str(accession)
+def cacheFileName(accession, cacheDir, dataType=annotationData):
+    # Combine the base cache area plus the specific data type being cached with the accession id.
+
+    # If the cache directory does not exist, create it.
+    os.makedirs(os.path.join(cacheDir, cacheLocation[dataType]), exist_ok=True)
+    return os.path.join(cacheDir, cacheLocation[dataType], str(accession))
 
 def getAccessionStatus(accessionList):
     # Relies on Entrez.email being set; currently being done in __main__, maybe
-    # not the best place.
+    # not the best place?
 
     # Variable to store Entrez records for these IDs
     records = {}
+    # Flag to indicate we experienced an error retrieving data.
+    searchError = False
     
     # esummary doesn't take a python list of accession IDs, unfortunately.
     accessionQuery = ", ".join(accessionList)
 
+    logging.debug("Retrieving status for accession numbers: %s", accessionQuery)
     handle = Entrez.esummary(db="nuccore", id=accessionQuery, retmode="xml")
-    for record in Entrez.parse(handle):
-        # Add the record to our dict, indexed by accession number + version.
-        records[record['AccessionVersion']] = record
+    parsedRecords = Entrez.parse(handle)
+    try:
+        for record in parsedRecords:
+            records[record['AccessionVersion']] = record
+            logging.info("Found entry for %s", record['AccessionVersion'])
+            logging.debug("Entry data:\n%s", str(record))
+                
+    except StopIteration:
+        # we got the last record.  All done.
+        logging.info("No more accession information in status search.")
+    except RuntimeError as errorMessage:
+        if str(errorMessage).startswith("Invalid uid"):
+            # In this case, we got NO data.  Need to go back around and try again
+            # unless we only tried one ID and got this message.
+            searchError = True
+            records = {}
+            logging.info("Unable to find entry with this ID.  Error: %s", errorMessage)
+        else:
+            raise
 
+    if searchError and len(accessionList) > 1:
+        # If we don't know what accessionList entry resulted in the error, try them
+        # individually.
+        # This seems a bit recursive... and it is, but the single entry check will
+        # terminate the recursion after at most two levels.
+        for accession in accessionList:
+            records.update(getAccessionStatus([accession,]))
+
+    #
+    # Sanity checking
+    #
+    
     if len(accessionList) > len(records):
         # Since the accession number _is_ an identifier in nuccore, this
         # suggests something went wrong.  Not sure exception is right, but...
-        logging.warning("WARNING: not all accession IDs found in Entrez nuccore database!")
-        raise KeyError
+        logging.info("Not all accession IDs found in Entrez nuccore database!")
 
     for accession in accessionList:
         if accession not in records:
-            logging.info("WARNING: Asked for accession ID %s, but not in the response.  Possibly the request doesn't have a version number?", accession)
+            logging.info("Asked for accession ID %s, but not in the response.  Possibly the request doesn't have a version number?", accession)
 
     return records
 
-def retrieveAndCacheAnnotations(accessionList, cacheDir):
-    # Note that this should be called by getAnnotationsThroughCache() because
+def retrieveAndCacheGenomeData(accessionList, cacheDir, dataType=annotationData):
+    # Note that this should be called by getGenomeDataThroughCache() because
     # that will ensure that the accession number used is the versioned one,
     # rather than the unversioned one.
     
     # This is where the annotation data will be stored (indexed by accession
     # number, containing an array of lines of text.
-    annotationData = {}
+    resultData = {}
     
     for accession in accessionList:
         
@@ -103,72 +157,63 @@ def retrieveAndCacheAnnotations(accessionList, cacheDir):
         # the structure we will use.  This should let us cache meaningfully
         # and reduce load on the server by making ony one request for multiple
         # annotations. - JN - TODO FIXME XXX
-        handle = Entrez.efetch(db="nuccore", id=accession, rettype="gb", retmode="text") # retmode?
+        handle = Entrez.efetch(db="nuccore", id=accession, rettype=dataType, retmode="text") # retmode?
 
-        annotationData[accession] = handle.read()
+        resultData[accession] = handle.read()
 
         handle.close()
 
-        logging.info("Retrieved new data for %s", accession)
+        logging.info("Retrieved new %s data for %s", dataType, accession)
 
         cacheFile = None
 
         try:
-            cacheFile = open(cacheFileName( accession, cacheDir),"w")
-            cacheFile.write(annotationData[accession])
+            cacheFile = open(cacheFileName( accession, cacheDir, dataType),"w")
+            cacheFile.write(resultData[accession])
             cacheFile.close()
-            logging.info("Successfully cached %s", accession)
+            logging.info("Successfully cached %s into %s", accession, cacheFileName( accession, cacheDir, dataType))
 
         except OSError:
             if cacheFile is not None:
                 cacheFile.close()
             logging.warning("WARNING: could not write cache file to cache directory for %s.  File permissions or a directory not existing could cause this.", accession)
-            if os.access(cacheFileName(accession, cacheDir), os.W_OK):  # If we have access to the cache file, then
+            if os.access(cacheFileName(accession, cacheDir, dataType), os.W_OK):  # If we have access to the cache file, then
                 # Remove the cache file because it is in an unknown state.
                 os.remove(cacheFileName(accession, cacheDir))
-            
-    '''
-    for record in Entrez.parse(handle):
-        annotationData[record.name] = record
-        cacheFiles[record.name].write(record)
-
-    for file in cacheFiles.values():
-        file.close()
-    '''
 
     for accession in accessionList:
-        if not accession in annotationData:  # If the info for this accession number wasn't retrieved, then
+        if not accession in resultData:  # If the info for this accession number wasn't retrieved, then
             logging.info("WARNING: could not retrieve %s from NCBI; it is not available for annotation", accession)
-            if os.access(cacheFileName(accession, cacheDir), os.W_OK):  # If we have access to the cache file, then
+            if os.access(cacheFileName(accession, cacheDir, dataType), os.W_OK):  # If we have access to the cache file, then
                 # Remove the cache file because it is out of date.
-                os.remove(cacheFileName(accession, cacheDir))
+                os.remove(cacheFileName(accession, cacheDir, dataType))
 
-    return annotationData
+    return resultData
 
 
-def findCachedAnnotation(accessionList, cacheDir):
+def findCachedGenomeData(accessionList, cacheDir, dataType=annotationData):
     # The actual annotation data retrieved from the cache
-    annotationData = {}
+    resultData = {}
 
     # The status of the cache file, e.g. file changed date.
-    annotationStatus = {}
+    resultStatus = {}
 
     cacheFile = None
     
     for accession in accessionList:
         try:
-            cacheName = cacheFileName(accession, cacheDir)
+            cacheName = cacheFileName(accession, cacheDir, dataType=dataType)
 
             # Find the creation time of the cached file.  Later it may
             # make sense to see if there is relevant info in the cache
             # file itself.
-            annotationStatus[accession] = {
+            resultStatus[accession] = {
                 'cacheTimeEpoch': os.stat(cacheName).st_ctime, }
             
             cacheFile = open(cacheName,"r")
-            annotationData[accession] = cacheFile.read()
+            resultData[accession] = cacheFile.read()
             cacheFile.close()
-            logging.debug("Found %s in the cache", accession)
+            logging.debug("Found %s in the cache at %s", accession, cacheName)
             
         except OSError:
             # If there is an OS error reading the cached data, we delete
@@ -181,11 +226,11 @@ def findCachedAnnotation(accessionList, cacheDir):
 
             # Make sure we don't reference this accession number in our
             # output / returned data; we don't have it in our cache.
-            annotationData.pop(accession, None)
-            annotationStatus.pop(accession, None)
+            resultData.pop(accession, None)
+            resultStatus.pop(accession, None)
             logging.debug("Error retrieving cached value for %s", accession)
 
-    return (annotationStatus, annotationData)
+    return (resultStatus, resultData)
 
 
 def cacheCurrent(annotation, databaseStatus, maxCacheDays):
@@ -218,7 +263,7 @@ def cacheCurrent(annotation, databaseStatus, maxCacheDays):
              datetime.date.today() - cachedDate < maxCachedLifetime )
 
             
-def getAnnotationsThroughCache(accessionList, cacheDir, maxCacheDays):
+def getGenomeDataThroughCache(accessionList, cacheDir, maxCacheDays, dataType=annotationData):
     # Passed a list of accession numbers, return annotation information for
     # each, making sure that 1) if the information must be retrieved from a
     # remote source, it is cached, and 2) if it has already been cached and
@@ -232,10 +277,10 @@ def getAnnotationsThroughCache(accessionList, cacheDir, maxCacheDays):
     # We are getting the cached status of the official accession number which
     # includes the version.  Note this may be different from the list passed
     # to this function.
-    (cachedStatus, cachedAnnotations) = findCachedAnnotation(databaseStatus.keys(), cacheDir)
+    (cachedStatus, cachedData) = findCachedGenomeData(databaseStatus.keys(), cacheDir, dataType=dataType)
     
     for accession in databaseStatus.keys():
-        if accession in cachedAnnotations:
+        if accession in cachedData:
             if cacheCurrent(cachedStatus[accession],databaseStatus[accession], maxCacheDays):
                 # If we have a cached annotation, and it is current, then skip
                 # this retrieval.
@@ -245,18 +290,18 @@ def getAnnotationsThroughCache(accessionList, cacheDir, maxCacheDays):
         # Otherwise we didn't have this cached, or it was out of date somehow.
         accessionListToRetrieve.append(accession)
 
-    retrievedAnnotations = retrieveAndCacheAnnotations(accessionListToRetrieve, cacheDir)
+    retrievedData = retrieveAndCacheGenomeData(accessionListToRetrieve, cacheDir, dataType)
 
     for accession in accessionListToRetrieve:
-        if not accession in retrievedAnnotations:
+        if not accession in retrievedData:
             # Remove the cached data (if any), because it is invalid.
-            cachedAnnotations.pop(accession, None)
+            cachedData.pop(accession, None)
             logging.info("Previously cached data for %s, now invalid, but no current data for this accession ID", accession)
 
 
     # Return the cached annotations overwritten by any that were more recently
     # retrieved.
-    return cachedAnnotations | retrievedAnnotations
+    return { **cachedData, **retrievedData }
 
 
 
@@ -273,8 +318,9 @@ def getAnnotationsThroughCache(accessionList, cacheDir, maxCacheDays):
 
 if __name__ == "__main__":
 
-    Entrez.email="ksnp-dev@kissake.net"
-    Entrez.tool="kSNP4"
+    Entrez.email = "ksnp-dev@kissake.net"
+    Entrez.tool = "kSNP4"
+    outputData = {}
 
     options = parseCommandline()
 
@@ -283,11 +329,17 @@ if __name__ == "__main__":
     else:
         logging.basicConfig(format='%(levelname)s:%(message)s')
 
-    # If the cache directory does not exist, create it.
-    os.makedirs(options.cachedir, exists_ok=True)
-    
-    annotations = getAnnotationsThroughCache(options.accessionList, options.cachedir, options.maxcachedays)
+    logging.debug('Commandline options, as parsed: %s', str(options))
 
-    for annotation in annotations.values():
-        sys.stdout.write(annotation)
-    
+    if options.responses == None:
+        responses = ['gb'] # Default to return annotations if not specified.
+    else:
+        responses = options.responses
+
+    for dataType in responses:
+        outputData[dataType] = getGenomeDataThroughCache(options.accessionList, options.cachedir, options.maxcachedays, dataType=dataType)
+        
+    for outputSection in outputData.keys():
+        for (accession, output) in outputData[outputSection].items():
+            logging.debug("Outputting %s for %s", outputSection, accession)
+            sys.stdout.write(output)
