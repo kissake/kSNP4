3bf231d3b92494c860c6d301e69735c814f1ad7e
github_dev@kissake.net <github_dev@kissake.net>
2022-09-09 13:11:11 -0400

    Reorganized to make code more accessible.

--- partitionKmers.py
+++ partitionKmers.py
@@ -18,7 +18,7 @@ def getPartitionKmersArgs( override = None ):
     parser = argparse.ArgumentParser(description=description, epilog = example,
                             formatter_class=argparse.RawDescriptionHelpFormatter)
     
-    parser.add_argument('--partition', help='The file containing (one-per-line) the kmer prefix to use as the divider between files', default=sys.stdin)
+    parser.add_argument('--partition', help='The file containing (one-per-line) the kmer prefix to use as the divider between files', default=None)
     parser.add_argument('--suffix', help='The suffix to use before the number indicating which part is being output.', default='.part')
     parser.add_argument('--col', help='The (whitespace separated) column where kmers are to be found.  Data will be partitioned based on the value of this column.', default=1)
     parser.add_argument('kmers', metavar='kmerFile', nargs='+', help='A list of kmers to be filtered into multiple partitins (probably for multiprocessing)')
@@ -29,6 +29,62 @@ def getPartitionKmersArgs( override = None ):
         return parser.parse_args()
     else:
         return parser.parse_args(override)
+
+
+def partitionKmers(filename, fileSuffix, partitionsList, sortColumn=1, bufferSize=32*1024):
+
+    outFiles = {}
+    outFilesList = []
+    input = open(filename, 'r')
+
+    count = 0
+
+    for partition in partitionsList:
+        outFilesList.append(filename + fileSuffix + str(count))
+        # Open the just appended filename for output.
+        outFiles[partition] = open(outFilesList[-1],'w', bufferSize)
+        logging.debug("Opened partition file: %s", filename + fileSuffix + str(count))
+        # Set the output file to NOT use line buffering because it results
+        # in WAY too many writes.
+        outFiles[partition].reconfigure(line_buffering=False)
+        count = count + 1
+
+    block = input.readlines(bufferSize)
+    while len(block) > 0:
+        for line in block:
+            # Extract the kmer from the specified column (or the first, if not specified)
+            kmer = line.split(maxsplit=sortColumn)[-1]
+            if kmer == '': continue
+            # This line is... obscure looking.  It will write the line to
+            # the correct output file based on its content and the partitions
+            # that were input.
+            outFiles[partitions[bisect.bisect_right(partitions,kmer)]].write(line)
+        block = input.readlines(bufferSize)
+
+
+    for outputFile in outFiles.values():
+        outputFile.close()
+        
+    return outFilesList
+
+
+def parsePartitions(file=None):
+    if file is None:
+        partitionsFile = sys.stdin
+    else:
+        partitionsFile = open(file, 'r')
+        
+    # Note that 'sorted' is likely redundant; the output of guessPartitions.py
+    # will be sorted.
+    partitions = sorted([ line.strip() for line in partitionsFile.readlines() ])
+
+    # Our input is kmers; all kmers are less than ZZZZ.  This value is
+    # intended to provide a terminal bucket for kmers larger than the last
+    # partition value.
+    partitions.append('ZZZZ') 
+    partitionsFile.close()
+    return partitions
+
 
 
 if __name__ == '__main__':
@@ -53,53 +109,7 @@ if __name__ == '__main__':
     # People number lists starting at 1, Python numbers them starting at zero.
     searchColumn = int(options.col) - 1
 
-
-    if options.partition == sys.stdin:
-        partitionsFile = options.partition
-    else:
-        partitionsFile = open(options.partition, 'r')
-        
-    # Note that 'sorted' is likely redundant; the output of guessPartitions.py
-    # will be sorted.
-    partitions = sorted([ line.strip() for line in partitionsFile.readlines() ])
-
-    # Our input is kmers; all kmers are less than ZZZZ.  This value is
-    # intended to provide a terminal bucket for kmers larger than the last
-    # partition value.
-    partitions.append('ZZZZ') 
-    sys.stdin.close()
+    partitions = parsePartitions(options.partition)
 
     for file in options.kmers:
-        outFiles = {}
-        input = open(file, 'r')
-
-        count = 0
-
-        for partition in partitions:
-            outFiles[partition] = open(file + fileSuffix + str(count),'w', bufferSize)
-            logging.debug("Opened partition file: %s", file + fileSuffix + str(count))
-            # Set the output file to NOT use line buffering because it results
-            # in WAY too many writes.
-            outFiles[partition].reconfigure(line_buffering=False)
-            count = count + 1
-
-        block = input.readlines(bufferSize)
-        while len(block) > 0:
-            for line in block:
-                # Extract the kmer from the specified column (or the first, if not specified)
-                elements = line.split()
-                if len(elements) > searchColumn:
-                    kmer = elements[searchColumn]
-                else:
-                    # This was a blank or otherwise insufficiently long line.
-                    continue
-                # This line is... obscure looking.  It will write the line to
-                # the correct output file based on its content and the partitions
-                # that were input.
-                outFiles[partitions[bisect.bisect_right(partitions,kmer)]].write(line)
-            block = input.readlines(bufferSize)
-
-
-        for outputFile in outFiles.values():
-            outputFile.close()
-        input.close()
+        partitionKmers(file, fileSuffix, partitions, searchColumn, bufferSize)
